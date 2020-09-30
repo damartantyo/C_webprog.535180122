d44ff21d1f4f5240182f0a8522f155392111be9e
Anggito <damartantyo@gmail.com>
2020-10-01 04:33:36 +0700

    Pertemuan 3&4

---
+++ Pertemuan 4/scrpt1.js
@@ -0,0 +1,7 @@
+function changeLine1Style() {
+    document.getElementById("cLine1").className = "MakeRed";
+}
+
+function ChangeStyle(id, classname){
+    document.getElementById(id).className = classname;
+}
