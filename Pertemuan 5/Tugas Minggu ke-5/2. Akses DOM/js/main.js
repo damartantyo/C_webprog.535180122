601ef087efaab26673d207dceefc775435d321a5
Anggito <damartantyo@gmail.com>
2020-10-04 13:59:01 +0700

    Pertemuan 5

---
+++ Pertemuan 5/Tugas Minggu ke-5/2. Akses DOM/js/main.js
@@ -0,0 +1,23 @@
+function myUniversal() {
+    document.querySelector("*").style.backgroundColor = "red";
+  }
+
+  function myType() {
+    document.querySelector("h1").style.backgroundColor = "blue";
+  }
+  function myType() {
+    document.querySelector("h1").style.backgroundColor = "blue";
+  }
+  function myClass() {
+    document.querySelector(".container").style.backgroundColor = "yellow";
+  }
+  function myId() {
+    document.querySelector("#logo").style.backgroundColor = "blue";
+  }
+  function myAttribute() {
+    document.querySelector("a[target]").style.backgroundColor = "red";
+  }
+  function myGroup() {
+    document.querySelector("h2, h1").style.backgroundColor = "red";
+  }
+  
